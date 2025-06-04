3614c9be49930420334f78becec6d3fc05c98340
Vighnesh-code <vighneshzagade4@gmail.com>
2025-06-04 14:40:00 +0000

    implemented subscription routes & controllers

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -1,28 +1,36 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) =>
   res.send({ title: "GET all subscriptions" })
 );
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "Create subscriptions" })
-);
-subscriptionRouter.get("/:id", (req, res) =>
-  res.send({ title: "GET subscription details" })
-);
+
+subscriptionRouter.post("/", authorize, createSubscription);
+
+subscriptionRouter.get("/:id", authorize, getUserSubscriptions);
+
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "Update Subscription" })
 );
+
 subscriptionRouter.delete("/:id", (req, res) =>
   res.send({ title: "Delete subscriptions" })
 );
+
 subscriptionRouter.get("/user/:id", (req, res) =>
   res.send({ title: "Get all user subscriptions" })
 );
+
 subscriptionRouter.put("/:id/cancel", (req, res) =>
   res.send({ title: "CANCEL subscription" })
 );
+
 subscriptionRouter.get("/upcoming-renewals", (req, res) =>
   res.send({ title: "GET upcoming renewals" })
 );
